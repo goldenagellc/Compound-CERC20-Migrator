38b7eb66a89756f60f5ae89e33678a78c6d23987
hayden Shively <haydenshively@gmail.com>
2021-03-23 01:10:08 -0500

    Make interface more general for future migrations

--- ethereum/contracts/CERC20Migrator.sol
+++ ethereum/contracts/CERC20Migrator.sol
@@ -20,81 +20,89 @@ import "./external/keeperdao/ILiquidityPool.sol";
 import "./external/IWETH.sol";
 
 
-contract WBTCMigrator is FlashLoanReceiverBase {
+contract CERC20Migrator is FlashLoanReceiverBase {
     using SafeERC20 for IERC20;
 
     event Migrated(address indexed account, uint256 underlyingV1, uint256 underlyingV2);
 
     address payable private constant KEEPER_LIQUIDITY_POOL = payable(0x35fFd6E268610E764fF6944d07760D0EFe5E40E5);
     address private constant KEEPER_BORROW_PROXY = 0xde92742213FEa5f78c6840B6EcBf214115ea8002;
-    address private constant WBTC = 0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599;
-    address private constant CWBTC1 = 0xC11b1268C1A384e55C48c2391d8d480264A3A7F4;
-    address private constant CWBTC2 = 0xccF4429DB6322D5C611ee964527D42E5d685DD6a;
     address private constant CETH = 0x4Ddc2D193948926D02f9B1fE9e1daa0718270ED5;
 
-    Comptroller public immutable comptroller;
+    Comptroller public immutable COMPTROLLER;
 
-    UniswapAnchoredView public immutable priceOracle;
+    UniswapAnchoredView public immutable PRICEORACLE;
+
+    address public immutable UNDERLYING;
+
+    address public immutable CTOKENV1;
+
+    address public immutable CTOKENV2;
 
     receive() external payable {}
 
-    constructor(ILendingPoolAddressesProvider _provider, address _comptroller) FlashLoanReceiverBase(_provider) {
-        comptroller = Comptroller(_comptroller);
-        priceOracle = UniswapAnchoredView(Comptroller(_comptroller).oracle());
+    constructor(ILendingPoolAddressesProvider _provider, Comptroller _comptroller, address _cTokenV1, address _cTokenV2) FlashLoanReceiverBase(_provider) {
+        COMPTROLLER = _comptroller;
+        PRICEORACLE = UniswapAnchoredView(_comptroller.oracle());
+
+        address underlying = CERC20Storage(_cTokenV1).underlying();
+        require(underlying == CERC20Storage(_cTokenV2).underlying(), "cTokens have different underlying ERC20s");
+        UNDERLYING = underlying;
+        CTOKENV1 = _cTokenV1;
+        CTOKENV2 = _cTokenV2;
 
         // Enter the cETH market now so that we don't have to do it ad-hoc during KeeperDAO loans
         address[] memory markets = new address[](1);
         markets[0] = CETH;
-        Comptroller(_comptroller).enterMarkets(markets);
+        _comptroller.enterMarkets(markets);
     }
 
     /**
-     * @notice Like `migrate()`, allows msg.sender to migrate collateral from
-     *      cWBTCv1 to cWBTCv2, so long as msg.sender has already approve this contract
-     *      to transfer their cWBTCv1.
+     * @notice Like `migrate()`, allows msg.sender to migrate collateral from v1 to v2,
+     *      so long as msg.sender has already approved this contract to transfer their v1.
      *
      *      This version of the function returns early if it detects that msg.sender can't
-     *      be migrated. It also looks for WBTC dust after the transaction, and if any
+     *      be migrated. It also looks for UNDERLYING dust after the transaction, and if any
      *      exists it will be sent back to msg.sender
      *
-     * @param gasOptimized When true, borrow WBTC directly (from AAVE, 0.09% fee).
-     *      When false, get WBTC indirectly (from KeeperDAO, higher gas usage).
+     * @param gasOptimized When true, borrow UNDERLYING directly (from AAVE, 0.09% fee).
+     *      When false, get UNDERLYING indirectly (from KeeperDAO, higher gas usage).
      */
     function migrateWithExtraChecks(bool gasOptimized) external {
-        if (CERC20(CWBTC1).balanceOf(msg.sender) == 0) return;
+        if (CERC20(CTOKENV1).balanceOf(msg.sender) == 0) return;
 
-        ( , , uint256 shortfall) = comptroller.getAccountLiquidity(msg.sender);
+        ( , , uint256 shortfall) = COMPTROLLER.getAccountLiquidity(msg.sender);
         if (shortfall != 0) return;
 
-        address[] memory enteredMarkets = comptroller.getAssetsIn(msg.sender);
+        address[] memory enteredMarkets = COMPTROLLER.getAssetsIn(msg.sender);
         for (uint256 i = 0; i < enteredMarkets.length; i++) {
-            if (enteredMarkets[i] != CWBTC2) continue;
+            if (enteredMarkets[i] != CTOKENV2) continue;
 
             migrate(gasOptimized);
 
-            uint256 dust = IERC20(WBTC).balanceOf(address(this));
-            if (dust != 0) IERC20(WBTC).transfer(msg.sender, dust);
+            uint256 dust = IERC20(UNDERLYING).balanceOf(address(this));
+            if (dust != 0) IERC20(UNDERLYING).transfer(msg.sender, dust);
         }
     }
 
     /**
-     * @notice Allows msg.sender to migrate collateral from cWBTCv1 to cWBTCv2, so long
-     *      as msg.sender has already approve this contract to transfer their cWBTCv1.
+     * @notice Allows msg.sender to migrate collateral from v1 to v2, so long as msg.sender has
+     *      already approved this contract to transfer their v1.
      *
      *      WARNING: This is made possible by AAVE flash loans, which means migration will incur
-     *      a 0.09% loss in underlying WBTC
+     *      a 0.09% loss in underlying UNDERLYING if gasOptimized=true
      *
-     * @param gasOptimized When true, borrow WBTC directly (from AAVE, 0.09% fee).
-     *      When false, get WBTC indirectly (from KeeperDAO, higher gas usage).
+     * @param gasOptimized When true, borrow UNDERLYING directly (from AAVE, 0.09% fee).
+     *      When false, get UNDERLYING indirectly (from KeeperDAO, higher gas usage).
      */
     function migrate(bool gasOptimized) public {
-        uint256 supplyV1 = CERC20(CWBTC1).balanceOf(msg.sender);
+        uint256 supplyV1 = CERC20(CTOKENV1).balanceOf(msg.sender);
         require(supplyV1 > 0, "0 balance no migration needed");
-        require(IERC20(CWBTC1).allowance(msg.sender, address(this)) >= supplyV1, "Please approve for cWBTCv1 transfers");
+        require(IERC20(CTOKENV1).allowance(msg.sender, address(this)) >= supplyV1, "Please approve for v1 cToken transfers");
 
         // fetch the flash loan premium from AAVE. (ex. 0.09% fee would show up as `9` here)
         uint256 premium = LENDING_POOL.FLASHLOAN_PREMIUM_TOTAL();
-        uint256 exchangeRateV1 = CERC20(CWBTC1).exchangeRateCurrent();
+        uint256 exchangeRateV1 = CERC20(CTOKENV1).exchangeRateCurrent();
 
         uint supplyV2Underlying;
 
@@ -102,13 +110,13 @@ contract WBTCMigrator is FlashLoanReceiverBase {
             supplyV2Underlying = supplyV1 * exchangeRateV1 * (10_000 - premium) / 1e22;
             bytes memory params = abi.encode(msg.sender, supplyV1);
 
-            initiateAAVEFlashLoan(WBTC, supplyV2Underlying, params);
+            initiateAAVEFlashLoan(UNDERLYING, supplyV2Underlying, params);
 
         } else {
             supplyV2Underlying = supplyV1 * exchangeRateV1 / 1e18;
-            ( , uint256 collatFact, ) = comptroller.markets(CETH);
-            uint256 dollarsPerETH = priceOracle.getUnderlyingPrice(CETH);
-            uint256 dollarsPerBTC = priceOracle.getUnderlyingPrice(CWBTC1);
+            ( , uint256 collatFact, ) = COMPTROLLER.markets(CETH);
+            uint256 dollarsPerETH = PRICEORACLE.getUnderlyingPrice(CETH);
+            uint256 dollarsPerBTC = PRICEORACLE.getUnderlyingPrice(CTOKENV1);
             uint256 requiredETH = supplyV2Underlying * 1e18 * dollarsPerBTC / dollarsPerETH / collatFact;
 
             initiateKeeperFlashloan(msg.sender, requiredETH, supplyV1, supplyV2Underlying - 1);
@@ -117,21 +125,21 @@ contract WBTCMigrator is FlashLoanReceiverBase {
         emit Migrated(msg.sender, supplyV1 * exchangeRateV1 / 1e18, supplyV2Underlying);
     }
 
-    /// @dev When this is called, contract's WBTC balance should be _supplyV2Underlying. After this has run,
-    ///      the contract's WBTC balance will be _supplyV1 * exchangeRateV1.
+    /// @dev When this is called, contract's UNDERLYING balance should be _supplyV2Underlying. After this has run,
+    ///      the contract's UNDERLYING balance will be _supplyV1 * exchangeRateV1.
     function flashloanInner(
         address _account,
         uint256 _supplyV1,
         uint256 _supplyV2Underlying
     ) internal {
         // Mint v2 tokens and send them to _account
-        IERC20(WBTC).approve(CWBTC2, _supplyV2Underlying);
-        require(CERC20(CWBTC2).mint(_supplyV2Underlying) == 0, "Failed to mint cWBTCv2");
-        require(IERC20(CWBTC2).transfer(_account, IERC20(CWBTC2).balanceOf(address(this))), "Failed to send cWBTCv2");
+        IERC20(UNDERLYING).approve(CTOKENV2, _supplyV2Underlying);
+        require(CERC20(CTOKENV2).mint(_supplyV2Underlying) == 0, "Failed to mint v2 cToken");
+        require(IERC20(CTOKENV2).transfer(_account, IERC20(CTOKENV2).balanceOf(address(this))), "Failed to send v2 cToken");
 
         // Pull and redeem v1 tokens from _account
-        require(IERC20(CWBTC1).transferFrom(_account, address(this), _supplyV1), "Failed to receive cWBTCv1");
-        require(CERC20(CWBTC1).redeem(_supplyV1) == 0, "Failed to redeem cWBTCv1");
+        require(IERC20(CTOKENV1).transferFrom(_account, address(this), _supplyV1), "Failed to receive v1 cToken");
+        require(CERC20(CTOKENV1).redeem(_supplyV1) == 0, "Failed to redeem v1 cToken");
     }
 
     /// @dev Meant to be called by AAVE Lending Pool, but be careful since anyone might call it
@@ -150,7 +158,7 @@ contract WBTCMigrator is FlashLoanReceiverBase {
         flashloanInner(account, supplyV1, amounts[0]);
         
         // Get ready to repay flashloan
-        IERC20(WBTC).approve(address(LENDING_POOL), amounts[0] + premiums[0]);
+        IERC20(UNDERLYING).approve(address(LENDING_POOL), amounts[0] + premiums[0]);
         // Finish up
         return true;
     }
@@ -180,16 +188,16 @@ contract WBTCMigrator is FlashLoanReceiverBase {
     function keeperFlashloanCallback(address _account, uint256 _amountETH, uint256 _supplyV1, uint256 _supplyV2Underlying) external {
         require(msg.sender == KEEPER_BORROW_PROXY, "Flashloan initiated by outsider");
 
-        // Use the borrowed ETH to get WBTC
+        // Use the borrowed ETH to get UNDERLYING
         CEther(CETH).mint{value: _amountETH}();
-        require(CERC20(CWBTC2).borrow(_supplyV2Underlying) == 0, "Failed to borrow WBTC");
+        require(CERC20(CTOKENV2).borrow(_supplyV2Underlying) == 0, "Failed to borrow UNDERLYING");
 
         // Execute main migration logic
         flashloanInner(_account, _supplyV1, _supplyV2Underlying);
 
         // Get ready to repay flashloan (get original ETH back)
-        IERC20(WBTC).approve(CWBTC2, _supplyV2Underlying);
-        require(CERC20(CWBTC2).repayBorrow(_supplyV2Underlying) == 0, "Failed to repay WBTC borrow");
+        IERC20(UNDERLYING).approve(CTOKENV2, _supplyV2Underlying);
+        require(CERC20(CTOKENV2).repayBorrow(_supplyV2Underlying) == 0, "Failed to repay UNDERLYING borrow");
         require(CEther(CETH).redeemUnderlying(_amountETH) == 0, "Failed to retrieve original ETH");
         // Finish up
         KEEPER_LIQUIDITY_POOL.send(_amountETH + 1);
--- ethereum/migrations/2_initial_deployment.js
+++ ethereum/migrations/2_initial_deployment.js
@@ -1,23 +1,34 @@
-const WBTCMigrator = artifacts.require("WBTCMigrator");
+const CERC20Migrator = artifacts.require("CERC20Migrator");
 
 module.exports = (deployer, network, accounts) => {
-  let lendingPoolAddressesProvider;
-  let comptroller;
+  let LENDINGPOOLADDRESSESPROVIDER;
+  let COMPTROLLER;
+  let CTOKENV1;
+  let CTOKENV2;
 
   switch (network) {
     case "ganache-fork":
     case "ganache":
     case "production-fork":
     case "production":
-      lendingPoolAddressesProvider =
+      LENDINGPOOLADDRESSESPROVIDER =
         "0xB53C1a33016B2DC2fF3653530bfF1848a515c8c5";
-      comptroller = "0x3d9819210A31b4961b30EF54bE2aeD79B9c9Cd3B";
+      COMPTROLLER = "0x3d9819210A31b4961b30EF54bE2aeD79B9c9Cd3B";
+      CTOKENV1 = "0xC11b1268C1A384e55C48c2391d8d480264A3A7F4";
+      CTOKENV2 = "0xccF4429DB6322D5C611ee964527D42E5d685DD6a";
       break;
     default:
       console.error("Unknown network -- constructor args unspecified");
   }
 
-  deployer.deploy(WBTCMigrator, lendingPoolAddressesProvider, comptroller, {
-    from: accounts[0],
-  });
+  deployer.deploy(
+    CERC20Migrator,
+    LENDINGPOOLADDRESSESPROVIDER,
+    COMPTROLLER,
+    CTOKENV1,
+    CTOKENV2,
+    {
+      from: accounts[0],
+    }
+  );
 };
