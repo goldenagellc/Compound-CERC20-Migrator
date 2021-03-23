80449b71f97aea9fb1ff2e831b63c467e1ca425e
hayden Shively <haydenshively@gmail.com>
2021-03-22 19:31:33 -0500

    Finished contract

--- ethereum/contracts/WBTCMigrator.sol
+++ ethereum/contracts/WBTCMigrator.sol
@@ -20,27 +20,47 @@ import "./external/IWETH.sol";
 contract WBTCMigrator is FlashLoanReceiverBase {
     using SafeERC20 for IERC20;
 
+    event Migrated(address indexed account, uint underlyingV1, uint underlyingV2);
+
+    address private constant COMPTROLLER = 0x3d9819210A31b4961b30EF54bE2aeD79B9c9Cd3B;
     address private constant WBTC = 0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599;
     address private constant CWBTC1 = 0xC11b1268C1A384e55C48c2391d8d480264A3A7F4;
     address private constant CWBTC2 = 0xccF4429DB6322D5C611ee964527D42E5d685DD6a;
 
-    constructor(ILendingPoolAddressesProvider provider) FlashLoanReceiverBase(provider) {
+    constructor(ILendingPoolAddressesProvider provider) FlashLoanReceiverBase(provider) {}
 
-    }
-
+    /**
+     * @notice Like `migrate()`, allows anyone to migrate `account`'s collateral from
+     *      cWBTCv1 to cWBTCv2, so long as `account` has already approve this contract
+     *      to transfer their cWBTCv1.
+     *
+     *      This version of the function returns early if it detects that `account` can't
+     *      be migrated. It also looks for WBTC dust after the transaction, and if any
+     *      exists it will be sent back to `account`
+     *
+     * @param account The cWBTCv1 supplier to migrate
+     */
     function migrateWithExtraChecks(address account) external {
         if (CERC20(CWBTC1).balanceOf(account) == 0) return;
 
-        ( , uint liquidity1, uint shortfall1) = Comptroller(0x3d9819210A31b4961b30EF54bE2aeD79B9c9Cd3B).getAccountLiquidity(account);
-        if (shortfall1 != 0) return;
+        ( , , uint shortfall) = Comptroller(COMPTROLLER).getAccountLiquidity(account);
+        if (shortfall != 0) return;
 
         migrate(account);
-        ( , uint liquidity2, uint shortfall2) = Comptroller(0x3d9819210A31b4961b30EF54bE2aeD79B9c9Cd3B).getAccountLiquidity(account);
 
-        require(liquidity1 == liquidity2 && shortfall1 == shortfall2, "Oops. Account health mismatch");
-        require(IERC20(WBTC).balanceOf(address(this)) == 0, "Oops. Migration contract profited");
+        uint256 dust = IERC20(WBTC).balanceOf(address(this));
+        if (dust != 0) IERC20(WBTC).transfer(account, dust);
     }
 
+    /**
+     * @notice Allows anyone to migrate `account`'s collateral from cWBTCv1 to cWBTCv2, so long
+     *      as `account` has already approve this contract to transfer their cWBTCv1.
+     *
+     *      WARNING: This is made possible by AAVE flash loans, which means migration will incur
+     *      a 0.09% loss in underlying WBTC
+     *
+     * @param account The cWBTCv1 supplier to migrate
+     */
     function migrate(address account) public {
         uint256 supplyV1 = CERC20(CWBTC1).balanceOf(account);
         require(supplyV1 > 0, "0 balance no migration needed");
@@ -49,12 +69,15 @@ contract WBTCMigrator is FlashLoanReceiverBase {
         // fetch the flash loan premium from AAVE. (ex. 0.09% fee would show up as `9` here)
         uint256 premium = LENDING_POOL.FLASHLOAN_PREMIUM_TOTAL();
         uint256 exchangeRateV1 = CERC20(CWBTC1).exchangeRateCurrent();
-        uint256 supplyV2Underlying = supplyV1 * exchangeRateV1 * (10_000 - premium) / 10_000; // 18 extra decimals
+        uint256 supplyV2Underlying = supplyV1 * exchangeRateV1 * (10_000 - premium) / 1e22;
 
         bytes memory params = abi.encode(account, supplyV1);
-        initiateFlashLoan(WBTC, supplyV2Underlying / 1e18, params);
+        initiateFlashLoan(WBTC, supplyV2Underlying, params);
+
+        emit Migrated(account, supplyV1 * exchangeRateV1 / 1e18, supplyV2Underlying);
     }
 
+    /// @dev Meant to be called by AAVE Lending Pool, but be careful since anyone might call it
     function executeOperation(
         address[] calldata assets,
         uint256[] calldata amounts,
--- ethereum/test/wbtcmigrator.test.js
+++ ethereum/test/wbtcmigrator.test.js
@@ -1,9 +1,8 @@
-const { assert } = require("chai");
+const { assert, expect } = require("chai");
 const Big = require("big.js");
 
 let suppliers = require("./_suppliers.json");
-suppliers = [suppliers[18]];
-console.log(suppliers);
+const comptrollerabi = require("./_comptrollerabi.json");
 const cwbtcv1abi = require("./_cwbtcv1abi.json");
 
 const WBTCMigrator = artifacts.require("WBTCMigrator");
@@ -26,8 +25,11 @@ contract("WBTCMigrator Test", (accounts) => {
   const maxUINT256 = "0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF";
 
   let wbtcMigrator;
+  let comptroller;
   let cWBTCV1;
+
   let excludedSuppliers = new Set();
+  let successCountMarketsEntered = 0;
 
   it("should approve for transfer", async () => {
     const promises = suppliers.map(async (supplier) => {
@@ -52,25 +54,72 @@ contract("WBTCMigrator Test", (accounts) => {
     );
   });
 
-  it("should migrate", async () => {
+  it("should enter market", async () => {
     const promises = suppliers.map(async (supplier) => {
-      if (excludedSuppliers.has(supplier)) return true;
+      if (excludedSuppliers.has(supplier)) return false;
+      await web3.unlockUnknownAccount(supplier);
 
-      const tx = await wbtcMigrator.migrateWithExtraChecks(supplier, { from: supplier });
-      assert.equal(tx.receipt.status, 1);
+      try {
+        const method = comptroller.methods.enterMarkets([
+          "0xccf4429db6322d5c611ee964527d42e5d685dd6a",
+        ]);
+        const tx = method.send({ from: supplier });
+        const receipt = await tx;
 
-      const events = tx.receipt.rawLogs;
-      assert.isTrue(events.length == 25 || events.length == 23);
-
-      return tx.receipt.status == 1
+        assert.equal(receipt.status, 1);
+        return receipt.status == 1;
+      } catch (e) {
+        console.log(e.message);
+        excludedSuppliers.add(supplier);
+        return false;
+      }
     });
 
     const successes = await Promise.all(promises);
-    console.log(`Failed ${successes.reduce((a, b) => a + Number(!b), 0)} times`);
+    successCountMarketsEntered = successes.reduce((a, b) => a + Number(b), 0);
+    console.log(`Succeeded ${successCountMarketsEntered} times`);
+  });
+
+  it("should migrate", async () => {
+    const promises = suppliers.map(async (supplier) => {
+      if (excludedSuppliers.has(supplier)) return false;
+
+      const tx = await wbtcMigrator.migrateWithExtraChecks(supplier, {
+        from: supplier,
+      });
+      assert.equal(tx.receipt.status, 1);
+
+      // if anything happened...
+      if (tx.receipt.rawLogs.length > 0) {
+        // then we expect a migration to have happened...
+        numLogs = tx.receipt.rawLogs.length;
+        assert.isTrue(numLogs == 27 || numLogs == 25);
+
+        const migration = tx.receipt.logs[0];
+        assert.equal(migration.logIndex, numLogs - 2);
+        assert.equal(migration.event, "Migrated");
+        assert.equal(migration.args.account, web3.utils.toChecksumAddress(supplier));
+
+        const underlyingV1 = migration.args.underlyingV1.toNumber();
+        const underlyingV2 = migration.args.underlyingV2.toNumber();
+        assert.isTrue(Math.round(10000 * underlyingV2 / underlyingV1).toFixed(0) === "9991");
+      }
+
+      return tx.receipt.status == 1;
+    });
+
+    const successes = await Promise.all(promises);
+    let successCount = successes.reduce((a, b) => a + Number(b), 0);
+    assert.equal(successCount, successCountMarketsEntered);
+    console.log(`Succeeded ${successCount} times`);
   });
 
   before(async () => {
     wbtcMigrator = await WBTCMigrator.deployed();
+    comptroller = new web3.eth.Contract(
+      comptrollerabi,
+      "0x3d9819210a31b4961b30ef54be2aed79b9c9cd3b"
+    );
     cWBTCV1 = new web3.eth.Contract(
       cwbtcv1abi,
       "0xc11b1268c1a384e55c48c2391d8d480264a3a7f4"
